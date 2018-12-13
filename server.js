000c2ed9e5aec8b429bc20858f18358ca571e329
Charles Ledvina <charles@indexdata.com>
2018-12-13 15:27:08 -0600

    Add some basic responses

--- server.js
+++ server.js
@@ -1,17 +1,65 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const fs = require('fs');
-const oaiRoot = '<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/ http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd"></OAI-PMH>'
+
+const convert = require('xml-js');
+resDate = new Date().toISOString();
+let jsonRes = {
+  _declaration: {
+    _attributes: {
+      version: '1.0',
+      encoding: 'utf-8'
+    }
+  },
+  OAI_PMH: {
+    _attributes: {
+      xmlns: 'http://www.openarchives.org/OAI/2.0/',
+      'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+      'xsi:schemaLocation': 'http://www.openarchives.org/OAI/2.0/ http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd'
+    },
+    responseDate: resDate
+  }
+}
+let resOpts = {compact: true, ignoreComment: true, spaces: 4};
 
 app.get('/oai/:connector', function (req, res) {
   const verb = req.query.verb;
-  // res.send(JSON.stringify(req.params) + JSON.stringify(req.query));
-  if (verb == 'listRecords') {
+  res.contentType('text/xml');
+  jsonRes.OAI_PMH.request = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+  switch(verb) {
+    case 'ListRecords':
+      jsonRes.OAI_PMH.ListRecords = {
+        record: {
+          header: {}
+        }
+      };
+      break;
+    case 'Identify':
+      jsonRes.OAI_PMH.Identify = {
+        repositoryName: 'Malaga adaptor service',
+        baseURL: req.protocol + '://' + req.get('host') + req.path,
+        protocolVersion: '2.0',
+        adminEmail: '',
+        earliestDatestamp: '',
+        deletedRecord: 'transient',
+        granularity: 'YYYY-MM-DD'
+      };
+      break;
+    default:
+      jsonRes.OAI_PMH.error = {
+        _attributes: { code: 'badVerb'},
+        _text: 'Verb not supported'
+      };
+  }
+
+  const xml = convert.js2xml(jsonRes, resOpts);
+  res.send(xml);
+
+  /* if (verb == 'listRecords') {
     let recs = fs.readFileSync('./euclid.xml', 'utf-8');
-    res.contentType('text/xml');
     res.send(recs);
-  } 
+  } */
 });
  
 app.listen(port, () => {
